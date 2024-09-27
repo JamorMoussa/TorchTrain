c70654061f5ba74393e9cd0062add1d237ac7b17
jamormoussa <moussajamorsup@gmail.com>
2024-09-27 18:18:15 +0100

    change test

--- test/test_trainer.py
+++ test/test_trainer.py
@@ -1,21 +1,53 @@
 import torch_train as ttn
-import torch 
+from torch_train.utils import OutputsResults
+
+import torch, torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+
+X = torch.randn(10000, 3)
+y = torch.mm(X, torch.Tensor([1, 2, 3]).view(-1, 1)) + 0.5 * torch.randn(10000, 1)
+
+
+dataset = TensorDataset(X, y)
+
+train_loader = DataLoader(dataset= dataset, shuffle=True, batch_size=32)
+
+model = nn.Sequential(
+    nn.Linear(3, 2),
+    nn.ReLU(),
+    nn.Linear(2, 1)
+)
+
+loss_fn = nn.MSELoss()
+
+opt = optim.SGD(model.parameters(), lr=0.01)
 
 configs = ttn.TorchTrainerConfigs(
-    device= torch.device("cuda")
+    device= torch.device("cpu"),
+    num_iters=100
 )
 
 trainer = ttn.TorchTrainer(configs= configs)
 
+trainer.set_model(model=model)
+trainer.set_lossfn(loss_fn=loss_fn)
+trainer.set_optimizer(optimizer=opt)
+trainer.set_data_loaders(train_loader=train_loader, test_loader=train_loader)
+
 
 @trainer.train_step
-def train_step(trainer: ttn.TorchTrainer, batch: torch.Tensor):
-    print(trainer.__class__.__name__)
-    print(trainer.configs.device)
+def train_step(model, loss_fn, batch, steps):
 
-@trainer.test_step
-def test_step(trainer: ttn.TorchTrainer, batch: torch.Tensor):
-    print(trainer.model)
+    inputs, labels = batch
 
+    outputs = model(inputs)
 
-trainer.train()
+    loss = loss_fn(outputs, labels)
+
+    return OutputsResults(
+        outputs= outputs, loss=loss
+    )
+
+trainer.train(num_iters=100)
